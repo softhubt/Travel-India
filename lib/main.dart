7e9b69eb49fe640ee71d8d45d581da3f6787bc35
Keval Motisariya <kevalmotisariya225@gmail.com>
2024-07-11 11:14:18 +0530

    Push

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:travelindia/Views/Splash_Section/splash_view.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
